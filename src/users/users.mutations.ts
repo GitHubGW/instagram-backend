5f1141682d8f70ab81f2190fa59128d0ce7150ba
GW <kowonp@gmail.com>
2022-02-09 19:13:50 +0900

    Login Mutation

--- src/users/users.mutations.ts
+++ src/users/users.mutations.ts
@@ -1,11 +1,17 @@
 import * as bcrypt from "bcrypt";
 import { User } from ".prisma/client";
 import prisma from "../prisma";
+import jwt from "jsonwebtoken";
 
 interface CreateAccountArgs {
   name: string;
   username: string;
   email: string;
+  password: string;
+}
+
+interface LoginArgs {
+  username: string;
   password: string;
 }
 
@@ -25,6 +31,30 @@ export default {
       } catch (error) {
         console.log("createAccount error");
         return null;
+      }
+    },
+
+    login: async (_: any, { username, password }: LoginArgs) => {
+      try {
+        const foundUser: User | null = await prisma.user.findFirst({ where: { username } });
+
+        if (foundUser === null) {
+          return { ok: false, message: "존재하지 않는 유저입니다." };
+        }
+
+        const isPasswordMatch: boolean = await bcrypt.compare(password, foundUser.password);
+
+        if (isPasswordMatch === false) {
+          return { ok: false, message: "잘못된 비밀번호입니다." };
+        }
+
+        const token: string = await jwt.sign({ userId: foundUser.id, username: foundUser.username, email: foundUser.email }, process.env.JWT_SECRET_KEY as string, {
+          expiresIn: 100,
+        });
+        return { ok: true, message: "로그인에 성공하였습니다.", token };
+      } catch (error) {
+        console.log("login error");
+        return { ok: false, message: "로그인에 실패하였습니다." };
       }
     },
   },
