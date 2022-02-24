d0a58c1ddf7a9915c458894a9415c3ac2b577927
GW <kowonp@gmail.com>
2022-02-25 02:41:47 +0900

    Set onConnect and onDisconnect on the Subscription server

--- src/server.ts
+++ src/server.ts
@@ -5,21 +5,42 @@ import { ApolloServer, ExpressContext } from "apollo-server-express";
 import { graphqlUploadExpress } from "graphql-upload";
 import { createServer, Server } from "http";
 import { execute, subscribe } from "graphql";
-import { SubscriptionServer } from "subscriptions-transport-ws";
+import { ConnectionContext, SubscriptionServer } from "subscriptions-transport-ws";
 import { User } from ".prisma/client";
 import { handleGetLoggedInUser, handleCheckLogin } from "./users/users.utils";
 import prisma from "./prisma";
 import schema from "./schema";
 import pubsub from "./pubsub";
 
+interface ConnectionParams {
+  token?: string;
+  "content-type"?: string;
+}
+
 const startServer = async (): Promise<void> => {
   const app: Express = express();
-  app.use(morgan("dev"));
+  // app.use(morgan("dev"));
   app.use(graphqlUploadExpress());
   app.use("/uploads", express.static("uploads"));
 
   const httpServer: Server = createServer(app);
-  const subscriptionServer: SubscriptionServer = SubscriptionServer.create({ schema, execute, subscribe }, { server: httpServer, path: "/graphql" });
+  const subscriptionServer: SubscriptionServer = SubscriptionServer.create(
+    {
+      schema,
+      execute,
+      subscribe,
+      async onConnect({ token }: ConnectionParams, webSocket: any, context: ConnectionContext) {
+        if (token === undefined) {
+          throw new Error("토큰이 존재하지 않기 때문에 Subscription Server에 연결할 수 없습니다.");
+        }
+
+        const foundUser: User | null = await handleGetLoggedInUser(token);
+        return { loggedInUser: foundUser };
+      },
+      onDisconnect(webSocket: any, context: ConnectionContext) {},
+    },
+    { server: httpServer, path: "/graphql" }
+  );
   const apolloServer: ApolloServer<ExpressContext> = new ApolloServer({
     schema,
     context: async ({ req }) => {
